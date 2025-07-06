9e8d41d50939273c604917916e30db701fc5b315
WeiTeckTan <106325397+WeiTeckTan@users.noreply.github.com>
2025-07-06 20:55:55 +0800

    integrate:shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,14 @@
-import { Card } from '@repo/ui/src/card';
+import { Button } from '@repo/ui/components/button';
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <Card title="test">test out tailwind ui</Card>
+      <Button>this is a button</Button>
+      <Button variant="destructive">this is a button</Button>
+      <Button variant="outline">this is a button</Button>
+      <Button variant="secondary">this is a button</Button>
+      <Button variant="ghost">this is a button</Button>
+      <Button variant="link">this is a button</Button>
     </main>
   );
 }
